b9547c73b47f98dfe55043437fd3a4ec8352bab9
Will Sampson <wjsampson@crimson.ua.edu>
2020-10-11 17:21:42 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace kms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wannaDie = 0;
+
+            while(wannaDie != 3)
+            {
+                Console.WriteLine("\nDoes will wanna die? Enter 1 for yes and 2 for no, or 3 to exit.\n");
+                wannaDie = int.Parse(Console.ReadLine());
+
+                switch(wannaDie)
+                {
+                    case 1:
+                    Console.WriteLine("Correct, will desires the sweet relief of death.");
+                    break;
+
+                    case 2:
+                    Console.WriteLine("HA SIKE ASS!!!! YES HE DOES!!!!");
+                    break;
+
+                    case 3:
+                    Console.WriteLine("Okay it's over, but will's feelings of wanting to take a bus to the face isn't over.");
+                    break;
+                    
+                    default:
+                    Console.WriteLine("You can't follow directions and will still wants to die.");
+                    break;
+                }
+            }
+        }
+    }
+}
